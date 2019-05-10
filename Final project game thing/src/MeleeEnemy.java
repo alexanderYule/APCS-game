3d730458a30651f3379f7cd015ead3cc0f7e1d0b
sumukhipandey <sumukhipandey@10.16.81.49>
2019-05-10 12:02:05 -0700

    javadoc for melee enemy and straucture. Very basic for no code.

--- Final project game thing/src/MeleeEnemy.java
+++ Final project game thing/src/MeleeEnemy.java
@@ -1,4 +1,8 @@
-
+/**
+ * One of the enemies whose behavior has certain common traits from the enemy class , but otherwise has its own characteristic behavior.
+ * code TBD
+ * @author sumukhipandey
+ */
 public class MeleeEnemy extends Enemy
 {
 	
--- Final project game thing/src/Structure.java
+++ Final project game thing/src/Structure.java
@@ -1,4 +1,8 @@
-
+/**
+ * Structures the obstacles in the room.
+ * code TBD
+ * @author sumukhipandey
+ */
 public class Structure {
 
 }
